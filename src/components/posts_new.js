e6dbe86e56007813bff53e03d7413a004927efda
DarkMass27 <jonathanscialpi@gmail.com>
2018-10-23 07:22:19 -0400

    adding form and error validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,66 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component{
+    
+    renderField(field){
+        return(
+            <div className = "form-group">
+                <label>{field.label}</label>
+                <input className = "form-control"
+                    type = "text"
+                    {...field.input}
+                />
+            </div>
+        )
+    }
+
     render(){
         return(
-            <div>
-                PostsNew!
-            </div>
+           <form>
+               <Field
+                label = "Title For Post"
+                name = "title"
+                component = {this.renderField}
+               />
+               <Field 
+                label = "categories"
+                name = "Categories"
+                component = {this.renderField}
+               />
+               <Field
+                label = "Post Content"
+                name = "content"
+                component = {this.renderField}
+               />
+           </form>
         )
     }
 }
 
-export default PostsNew;
+function validate(values){
+    // console.log(values) -> {title: 'asdf', categories: 'asdf', content: 'asdf'}
+    const errors = {};
+
+    // validate the inputs from 'values'
+    if (!values.title){
+        errors.title = "Enter a title!";
+    }
+
+    if(!values.categories){
+        errors.categories = "Enter some categories!";
+    }
+
+    if(!values.content){
+        errors.content = "Enter some content please"
+    }
+
+    // If errors is empty, the form is fine to submit
+    // If errors has *any* properties, redux form assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
